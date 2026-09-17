c5dd42c8fc9e7485c9360d6b6c4daad6ba2308b6
JohnTheArtist <johnr2d20@gmail.com>
2026-09-17 10:21:59 -0400

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,20 @@ public class Main {
     public static void main(String[] args) {
         //create two person object
 
-        Person s1 = new Person();
+        Person p1 = new Person();
 
-        Person s2 = new Person(72, 21, "black", "John Atkins", 140);
+//        Person p2 = new Person(72, 21, "black", "John Atkins", 140);
 
-        System.out.println(s2.name);
+
+        Person p2 = new Person(72, 21);
+
+        System.out.println(p2.age);
+
+
+        Person p3 = p2.Call_Private();
+
 
     }
+
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,5 @@
 
-
+//private and protected are not allowed as class access modifiers.
 
 public class Person {
     //instance variable
@@ -14,7 +14,9 @@ public class Person {
     double weight;
 
 
-    Person(){
+    //constructors can have private, protected, public access modifiers
+
+     Person(){
         height = 40;
         age = 10;
         color = "black";
@@ -23,9 +25,19 @@ public class Person {
     }
 
 
+    // protected members are accessible within the same package
+
+    protected Person(double height, int age){
+        this.height = 40;
+       this.age = 10;
+
+    }
+
+
+
     //Overloaded constructor
 
-    Person(double height, int age, String color, String name, double weight){
+    private Person(double height, int age, String color, String name, double weight){
         //parameters are local variables to the constructor
         this.height = height;
         this.age = age;
@@ -36,6 +48,14 @@ public class Person {
 
 
 
+    }
+
+    //setters and getters
+    //methods
+    public Person Call_Private(){
+        Person p3 = new Person(72, 21, "black", "John Atkins", 140);
+
+        return p3;
     }
 
 }
